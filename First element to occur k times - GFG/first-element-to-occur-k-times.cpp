a140acc55a886f7897c2bdf87d595b3e1d98194a
Kushagra Agrawal <94302748+Kushagra2024@users.noreply.github.com>
2023-04-15 22:06:15 +0530

    Added solution - LeetHub

--- First element to occur k times - GFG/first-element-to-occur-k-times.cpp
+++ First element to occur k times - GFG/first-element-to-occur-k-times.cpp
@@ -8,16 +8,21 @@ class Solution{
     public:
     int firstElementKTime(int a[], int n, int k)
     {
+        //finding the maximum element in the array
         int max_ = a[0];
         for(int i = 1; i < n; i++)
         {
             max_ = max(max_, a[i]);
         }
+        
+        //creating array of maximum element size
         int temp[max_] = {0};
         
+        //increament the element position value in the temp array by 1 and simultaneously check if it reached the required limit or not
         for(int i = 0; i < n; i++)
         {
             temp[a[i]-1]++;
+            //checking for the limit
             if(temp[a[i]-1] >= k)
             {
                 return a[i];
