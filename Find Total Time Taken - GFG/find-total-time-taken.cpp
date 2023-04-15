095f453ee0a22c08ff7dc1d145706d1f2fff02d3
Kushagra Agrawal <94302748+Kushagra2024@users.noreply.github.com>
2023-04-15 20:57:55 +0530

    Added solution - LeetHub

--- Find Total Time Taken - GFG/find-total-time-taken.cpp
+++ Find Total Time Taken - GFG/find-total-time-taken.cpp
@@ -36,17 +36,22 @@ class Solution
         int sum = 0;
         int temp[n] = {0};
         
-        //
+        //marking presence of 1st element
         temp[arr[0]-1] = 1;
+        
+        //iterating over the array starting from 2nd element
         for(int i = 1; i < n; i++)
         {
+            //checking the element's presence in the temp array
+            //if 1 then present => simply add value of time of that element position
+            //if 0 then absent => simply add 1 and mark its presence
             if(temp[arr[i]-1])
             {
-                sum += time[arr[i]-1];
+                sum += time[arr[i]-1];      //logic for true
             }
             else
             {
-                sum += 1;
+                sum += 1;                   //logic for false
                 temp[arr[i]-1] = 1;
             }
         }
