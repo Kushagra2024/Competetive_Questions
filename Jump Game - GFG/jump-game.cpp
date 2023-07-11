5fc250224667b41932b5aeb6211420606765271e
Kushagra Agrawal <94302748+Kushagra2024@users.noreply.github.com>
2023-07-11 21:59:40 +0530

    Added solution - LeetHub

--- Jump Game - GFG/jump-game.cpp
+++ Jump Game - GFG/jump-game.cpp
@@ -10,63 +10,63 @@ using namespace std;
 class Solution {
   public:
         //Approach 2: Peak-Valley approach
-    // int canReach(int A[], int N) 
-    // {
-    //     int reachable = 0;
+    int canReach(int A[], int N) 
+    {
+        int reachable = 0;
         
-    //     for(int i = 0; i < N; i++)
-    //     {
-    //         if(i > reachable)
-    //         {
-    //             return 0;
-    //         }
-    //         reachable = max(reachable, i + A[i]);
-    //     }
-    //     return 1;
-    // }
+        for(int i = 0; i < N; i++)
+        {
+            if(i > reachable)
+            {
+                return 0;
+            }
+            reachable = max(reachable, i + A[i]);
+        }
+        return 1;
+    }
     
     
         //Approach 1: Brute Force
-    int findMax(int arr[], int i, int j)
-    {
-        int max = i, k;
-        for(k = i; k <= j; k++)
-        {
-            if(arr[max] <= arr[k])
-            {
-                max = k;
-            }
-        }
-        return max;
-    }
+    // int findMax(int arr[], int i, int j)
+    // {
+    //     int max = i, k;
+    //     for(k = i; k <= j; k++)
+    //     {
+    //         if(arr[max] <= arr[k])
+    //         {
+    //             max = k;
+    //         }
+    //     }
+    //     return max;
+    // }
     
-    int canReach(int A[], int N) 
-    {
+    // int canReach(int A[], int N) 
+    // {
         
-        if(N == 1)
-        {
-            return 1;
-        }
+    //     if(N == 1)
+    //     {
+    //         return 1;
+    //     }
         
-        int i, j, size, size_index = 0;
+    //     int i, j, size, size_index = 0;
         
-        size = A[size_index];
-        while(size)
-        {
-            i = size_index + 1;
-            j = i-1 + size;
+    //     size = A[size_index];
+    //     while(size)
+    //     {
+    //         i = size_index + 1;
+    //         j = i-1 + size;
             
-            if(j >= (N-1))
-            {
-                return 1;
-            }
+    //         if(j >= (N-1))
+    //         {
+    //             return 1;
+    //         }
             
-            size_index = findMax(A, i, j);
-            size = A[size_index];
-        }
-        return 0;
+    //         size_index = findMax(A, i, j);
+    //         size = A[size_index];
+    //     }
+    //     return 0;
         
-    }
+    // }
 };
 
 //{ Driver Code Starts.
